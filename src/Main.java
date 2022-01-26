75d02ff1b73205370e010ec2dc41414c9fae3560
zybert <mikolaj.sykucki@gmail.com>
2022-01-26 23:07:37 +0100

    ćw 6

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* ćw.4 */
+        /* Tablice - cw_6 */
 
-        /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
+        /* zad1. stworzyć tablicę typu double 5-cio elementową,
+        przypisać do niej wartości i wyświetlić */
+        double tab[] = {1, 2, 3, 4, 5};
 
+        for (int i = 0; i < tab.length; i++){
+            System.out.println(tab[i] + " ");
+        }
 
     }
 }
