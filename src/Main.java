e938a287b4bd1abb43aad891153dac309932d24a
zybert <mikolaj.sykucki@gmail.com>
2022-01-26 23:20:32 +0100

    ćw 1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,75 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        /* Tablice - cw_6 */
+        /* cw.1 pętle while */
 
-        /* zad1. stworzyć tablicę typu double 5-cio elementową,
-        przypisać do niej wartości i wyświetlić */
-        double tab[] = {1, 2, 3, 4, 5};
+        /* zad 1 - Napisać za pomocą pętli do-while kod który będzie iterował od liczby 1000 w dół do 0.
+          Jeśli liczba będzie podzielna przez 2 ostatnie cyfry waszego indeksu (podać nr indesu z uczelni)
+          to dać komunikat "liczba XX podzielna przez YY" (XX-liczba podzielna YY-podzielnik)
+        */
+        System.out.println("Zadanie 1:");
 
-        for (int i = 0; i < tab.length; i++){
-            System.out.println(tab[i] + " ");
+        int i = 1000;
+        int dwieOstatnieCyfryIndexu=25;
+        int x =0;
+
+        do {
+            if(i%dwieOstatnieCyfryIndexu==0)
+            {
+                System.out.println("Liczba " + i + " podzielna przez " + dwieOstatnieCyfryIndexu);
+            }
+
+        } while(i--!=1);
+
+        /* zad 2
+        a) stworzyć tablicę typu int wielkosci 5, przypisac jej dowolne wartości,
+          przeiterować tablicę po elementach i wyswietlić, użyć pętli while
+        b) jak wyżej tylko dla tablicy typu double[],
+        c) jak wyżej dla tablicy String[], przeiterować i wyświetlić od ostatniego elementu
+        */
+        System.out.println("Zadanie 2: ");
+        int[] tab = {1, 2, 3, 5, 8};
+        int counter =0;
+
+        System.out.println("Wartości tablicy int: ");
+        while (counter< tab.length)
+        {
+            System.out.println(tab[counter++]);
+        }
+        double[] tabDouble = {1.01, 2.02, 3.03, 5.05, 8.08};
+        int counter2 =0;
+
+        System.out.println("Wartości tablicy double: ");
+        while (counter2< tabDouble.length)
+        {
+            System.out.println(tabDouble[counter2++]);
+        }
+
+
+        String[] stringTab = {"jeden", "dwa", "trzy", "cztery", "pięć"};
+        int counter3 =0;
+
+        System.out.println("Wartości tablicy string: ");
+        while (counter3< stringTab.length)
+        {
+            System.out.println(stringTab[counter3++]);
+        }
+
+
+        /* zad.3
+        a) sprawdzić jak zachowuje się pętla while gdy w jej warunku damy true --> while(true)
+        b) a jak gdy damy false --> while(false)
+        */
+        System.out.println("Zadanie 3:");
+
+        while(true)
+        {
+            System.out.println("Pętla nieskończona");
+        }
+        while(false)
+        {
+            System.out.println("Pętla się nie wykona");
         }
 
     }
