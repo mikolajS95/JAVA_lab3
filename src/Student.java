5c5218a42cc428b0dca241fc9bccd872f630f3e9
mikolajS95 <98489236+mikolajS95@users.noreply.github.com>
2022-01-31 23:24:00 +0100

    Create Student.java

---
+++ src/Student.java
@@ -0,0 +1,15 @@
+/* zad.2
+            a) Stworzyć klasę Student z polami prywatnymi
+            - Osoba osoba,
+            - WydzialEnum wydzial;
+            b) stworzyć enum WydzialEnum (w nowym pliku) z kilkoma polami,
+            c) stworzyc konstruktory, gettery i settery
+            d) nadpisać metodę toString aby zwracała wszystkie elementy ładnie przedstawione :)
+         */
+
+public class Student
+{
+    private Osoba osoba;
+    private WydzialEnum wydzial;
+
+}
